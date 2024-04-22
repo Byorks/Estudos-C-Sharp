2bd10a9a1df0631c4661d14aaa206f9584ce191d
VANESSA BYORK FERREIRA PINTO <52865485803@info.local>
2024-04-22 13:57:24 -0300

    feat: Primeira Tentativa Ex06Crime

--- Repos/Condicionais/Ex06Crime/Ex06Crime/Program.cs
+++ Repos/Condicionais/Ex06Crime/Ex06Crime/Program.cs
@@ -1,19 +1,48 @@
 ﻿string per1,per2,per3,per4,per5;
 int sim = 0;
 
+Console.WriteLine("Responda as perguntas com sim ou não.");
+
 Console.WriteLine("Telefonou para a vítima?");
 per1 = Console.ReadLine();
+if (per1 == "sim" || per1 == "Sim"){
+    sim++;
+}
 
 Console.WriteLine("Esteve no local do crime?");
 per2 = Console.ReadLine();
 
+if (per2 == "sim" || per2 == "Sim"){
+    sim++;
+}
+
 Console.WriteLine("Mora perto da vítima?");
 per3 = Console.ReadLine();  
+if (per3 == "sim" || per3 == "Sim"){
+    sim++;
 
 Console.WriteLine("Devia para a vítima?");
 per4 = Console.ReadLine();
 
+if (per4 == "sim" || per4 == "Sim"){
+    sim++;
+
 Console.WriteLine("Já trabalhou com a vítima?");
 per5 = Console.ReadLine();
+if (per5 == "sim" || per5 == "Sim"){
+    sim++;
+}
 
-if
+if (sim == 2)
+{
+    Console.WriteLine("Você é considerado Suspeito!");
+}
+else if (sim > 2 && sim <= 4) {
+
+    Console.WriteLine("Você é considerado Cúmplice!");
+}
+else if (sim == 5) {
+    Console.WriteLine("Você é culpado!");
+}
+
+Console.ReadKey();
