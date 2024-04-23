772a91a80a09e774f45c0b0746250395c86f1909
Byorks <vanessabyorksp@gmail.com>
2024-04-23 17:01:39 -0300

    Added - Ex06Crime2ndVer

---
+++ Repos/Condicionais/Ex06Crime2ndVer/Ex06Crime2ndVer/Program.cs
@@ -0,0 +1,52 @@
+﻿int sim = 0;
+String[] perguntas;
+
+Console.WriteLine("Responda as perguntas com sim ou não.");
+perguntas = new string[0]Console.ReadLine();
+
+Console.WriteLine("Telefonou para a vítima?");
+
+
+Console.WriteLine("Esteve no local do crime?");
+
+Console.WriteLine("Mora perto da vítima?");
+
+Console.WriteLine("Devia para a vítima?");
+
+
+Console.WriteLine("Já trabalhou com a vítima?");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if (sim == 2)
+{
+    Console.WriteLine("Você é considerado Suspeito!");
+}
+
+else if (sim > 2 && sim <= 4)
+{
+
+    Console.WriteLine("Você é considerado Cúmplice!");
+}
+
+else if (sim == 5)
+{
+    Console.WriteLine("Você é Culpado!");
+}
+
+Console.ReadKey();
