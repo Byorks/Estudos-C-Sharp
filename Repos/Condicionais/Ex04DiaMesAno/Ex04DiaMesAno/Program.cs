9f3c214afe62c619453032fd1c7c4fa4ba66aa0f
Byorks <vanessabyorksp@gmail.com>
2024-04-24 17:59:41 -0300

    Mod - Ex06Crime

--- Repos/Condicionais/Ex04DiaMesAno/Ex04DiaMesAno/Program.cs
+++ Repos/Condicionais/Ex04DiaMesAno/Ex04DiaMesAno/Program.cs
@@ -5,7 +5,7 @@ Console.WriteLine("Digite dia mes e ano, separados por um espaço");
 input = Console.ReadLine();
 
 // Com o Split consegue separar a string, e colocar dentro de um array, o parametro de separação fica entre ( ).
-string[] valores = input.Split('/');
+string[] valores = input.Split(' ');
 
 dia = int.Parse(valores[0]);
 
@@ -13,7 +13,7 @@ mes = int.Parse(valores[1]);
 
 ano = int.Parse(valores[2]);
 
-if (dia > 0 && dia < 32)
+if (dia > 0 && dia <= 31)
 {
     if (mes <= 12 && mes > 0)
     {
--- Repos/Condicionais/Ex06Crime2ndVer/Ex06Crime2ndVer/Program.cs
+++ Repos/Condicionais/Ex06Crime2ndVer/Ex06Crime2ndVer/Program.cs
@@ -1,24 +1,37 @@
 ﻿int sim = 0;
-String[] perguntas;
+// Criando um array  e colocando a largura, 5 iten, para colocar valores depois. 
+string[] perguntas = new string[5];
 
 Console.WriteLine("Responda as perguntas com sim ou não.");
-perguntas = Console.ReadLine();
+perguntas[0] = Console.ReadLine();
+
+// Como crirar um array e colocar um input dentro;
+//string[] perguntas = { Console.ReadLine()};
+
+Console.WriteLine("Indice 0 do array perguntas: " + perguntas[0]);
+perguntas[0] = Console.ReadLine();
 
 Console.WriteLine("Telefonou para a vítima?");
-
+perguntas[0] = Console.ReadLine();
 
 Console.WriteLine("Esteve no local do crime?");
+perguntas[0] = Console.ReadLine();
 
 Console.WriteLine("Mora perto da vítima?");
+perguntas[0] = Console.ReadLine();
 
 Console.WriteLine("Devia para a vítima?");
-
+perguntas[0] = Console.ReadLine();
 
 Console.WriteLine("Já trabalhou com a vítima?");
+perguntas[0] = Console.ReadLine();
 
 
 
-
+foreach (var item in perguntas)
+{
+    
+}
 
 
 
