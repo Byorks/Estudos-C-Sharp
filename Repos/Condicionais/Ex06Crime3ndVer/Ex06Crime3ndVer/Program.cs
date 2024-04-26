358d3a81f048b8ef2f18f38048e232694a82fd55
Byorks <vanessabyorksp@gmail.com>
2024-04-26 17:10:22 -0300

    Added - Ex06Crime3ndVer For

---
+++ Repos/Condicionais/Ex06Crime3ndVer/Ex06Crime3ndVer/Program.cs
@@ -0,0 +1,33 @@
+﻿int sim;
+string[] perguntas = new string[5];
+string[] respostas = new string[5];
+
+perguntas[0] ="Telefonou para a vítima?";
+perguntas[1] ="Esteve no local do crime?";
+perguntas[2] ="Mora perto da vítima?";
+perguntas[3] ="Devia para a vítima?";
+perguntas[4] ="Já trabalhou com a vítima?";
+
+sim = 0;
+
+Console.WriteLine("responda com sim ou não");
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine(perguntas[i]);
+    respostas[i] = Console.ReadLine();
+    // lembrar de falar qual é o tipo de objeto que queremos comparar no Equals, antes dos .Equals
+    if (string.Equals(respostas[i], "sim", StringComparison.OrdinalIgnoreCase))
+    {   
+        sim++;
+    }
+}
+
+if  (sim == 2)
+{
+    Console.WriteLine("Você é suspeito!");
+}
+else if (sim >= 3 || sim <= 4)
+{
+    Console.WriteLine("Você é cumplice!");
+}
