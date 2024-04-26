f89ce60494fe92460619dd6ed05884c2eb197905
Byorks <vanessabyorksp@gmail.com>
2024-04-26 14:33:41 -0300

    Fix - Adjusted Foreach and Method Equals with IgnoreCase

--- Repos/Condicionais/Ex06Crime2ndVer/Ex06Crime2ndVer/Program.cs
+++ Repos/Condicionais/Ex06Crime2ndVer/Ex06Crime2ndVer/Program.cs
@@ -18,7 +18,7 @@ respostas[0] = Console.ReadLine();
 Console.WriteLine("Indice 0 do array perguntas: Então " + respostas[0] + "!");
 
 Console.WriteLine("Telefonou para a vítima?");
-respostas[0] = Console.ReadLine(), new StringComparison.OrdinalIgnoreCase;
+respostas[0] = Console.ReadLine();
 
 Console.WriteLine("Esteve no local do crime?");
 respostas[1] = Console.ReadLine();
@@ -32,13 +32,22 @@ respostas[3] = Console.ReadLine();
 Console.WriteLine("Já trabalhou com a vítima?");
 respostas[4] = Console.ReadLine();
 
+// O Equals entra no if
+/* 
+ Foreach verifica cada entrada de uma list, indivídualmente
+ Neste caso, coloquei para verificar cada sim respondido nas perguntas, de maneira que verifique e atribua a uma variável , contabilizando quantos "sims foram respondidos"
 
+Equal é unm método e StringComparison.OrdinalIgonoreCase é o parametro
+Serve para fazer comparações que não devem ser sensíveis ao case;
+Retorna valor booleano. 
 
+*/
 foreach (string resps in respostas)
 {
-    
+
     Console.WriteLine("Resposta n°"+ num +" é: " + resps);
-    if (resps == "sim" || resps == "Sim"){
+
+    if (string.Equals(resps, "sim", StringComparison.OrdinalIgnoreCase)) {
         sim++;
         Console.WriteLine("If Foi Verificado!");
     }
@@ -57,7 +66,7 @@ else if (sim > 2 && sim <= 4)
     Console.WriteLine("\nVocê é considerado Cúmplice!");
 }
 
-else if (sim < 4)
+else if (sim > 4)
 {
     Console.WriteLine("\nVocê é Culpado!");
 }
