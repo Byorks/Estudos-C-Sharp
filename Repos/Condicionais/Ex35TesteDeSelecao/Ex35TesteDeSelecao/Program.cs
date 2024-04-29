fdcd7a23a7a97eac5fa752b5b577990eef6c41c3
Byorks <vanessabyorksp@gmail.com>
2024-04-29 13:58:47 -0300

    Added - Ex35TesteDeSelecao

---
+++ Repos/Condicionais/Ex35TesteDeSelecao/Ex35TesteDeSelecao/Program.cs
@@ -0,0 +1,30 @@
+﻿int A, B, C, D;
+
+Console.WriteLine("Insira 4 números inteiros separados por um espaço");
+string input =  Console.ReadLine();
+
+string[] numeros = input.Split(' ');
+
+A = int.Parse(numeros[0]);
+
+B = int.Parse(numeros[1]);
+
+C = int.Parse(numeros[2]);
+
+D = int.Parse(numeros[3]);
+
+if (B < C && D > A)
+{
+    int Ctemp = C + D;
+    int Atemp = A + B; 
+    if (Atemp < Ctemp)
+    {
+        if (C >= 0 && D >= 0) {
+            A = A % 2;
+            if (A == 0)
+            {
+                Console.WriteLine("Deu certo");
+            }
+        }
+    }
+}
