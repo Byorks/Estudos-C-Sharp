98eba146c8ed981e97d06ed142272e06be320e9b
VANESSA BYORK FERREIRA PINTO <52865485803@info.local>
2024-05-03 16:45:18 -0300

    Added - testes ari | debug

---
+++ Repos/TesteAritmeticos/Ex01FuncMath/Ex01FuncMath/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+double b, a, area, perimetro, diagonal;
+
+b = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);   
+a = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+area = b * a;
+
+perimetro = 2 * (b + a);
+
+//Sqrt raiz quadrada
+diagonal = Math.Sqrt(Math.Pow(b, 2.0) + Math.Pow(a, 2.0));
+
+Console.WriteLine("Area = " + area.ToString("F4", CultureInfo.InvariantCulture));
+Console.WriteLine("Perimentro = " + perimetro.ToString("F4", CultureInfo.InvariantCulture));
+Console.WriteLine("Diagonoal = " + diagonal.ToString("F4", CultureInfo.InvariantCulture));
