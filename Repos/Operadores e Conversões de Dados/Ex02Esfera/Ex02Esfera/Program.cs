76663a8a5e577d7e72c89e57faaa5bb966e20bee
VANESSA BYORK FERREIRA PINTO <52865485803@info.local>
2024-05-06 15:11:14 -0300

    Added - Ex2Esfera Done

---
+++ Repos/Operadores e Conversões de Dados/Ex02Esfera/Ex02Esfera/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+double raio, volume;
+double pi = 3.14159;
+
+
+Console.WriteLine("**** Esse programa calcula o volume de uma esfera ****");
+
+Console.WriteLine("Insira o raio da esfera: ");
+raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+volume = (4 / 3.0) * pi * Math.Pow(raio, 3);
+
+Console.WriteLine("Volume: " + volume.ToString("F3", CultureInfo.InvariantCulture));
