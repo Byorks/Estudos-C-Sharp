c90906065311540f174ffb29066e8b7b0ad623b2
VANESSA BYORK FERREIRA PINTO <52865485803@info.local>
2024-05-06 14:47:37 -0300

    Added - Operadores e Conversoes

---
+++ Repos/Operadores e Conversões de Dados/Ex01SalarioComBonus/Ex01SalarioComBonus/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+string nome;
+double salariofixo, totalvendas, ganhodomes;
+
+Console.Write("Insira o nome: \n");
+nome = Console.ReadLine();
+
+Console.Write("Insira o salario mensal:\n");
+salariofixo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.Write("Insira o ganho do mes\n");
+totalvendas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+ganhodomes = salariofixo + (totalvendas * 15 / 100);
+
+Console.WriteLine("Total: "+ ganhodomes.ToString("F2", CultureInfo.InvariantCulture));
+
