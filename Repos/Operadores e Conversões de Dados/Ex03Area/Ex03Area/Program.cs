892e32de80faed128ccc2425f9f2756ee904e2d9
VANESSA BYORK FERREIRA PINTO <52865485803@info.local>
2024-05-06 15:28:33 -0300

    ExArea modded

---
+++ Repos/Operadores e Conversões de Dados/Ex03Area/Ex03Area/Program.cs
@@ -0,0 +1,21 @@
+﻿double A, B, C;
+
+double[] calculos= new double[5];
+
+double pi = 3.14159;
+
+A = double.Parse(Console.ReadLine());
+
+B = double.Parse(Console.ReadLine());
+
+C = double.Parse(Console.ReadLine());
+
+//Triangulo base A altura C
+calculos[0] = (A * C) / 2;
+
+//Area de circulo raio C
+calculos[1] = pi * (C * C);
+
+//area do trapézio com A e B por bases e C por altura
+calculos[2] = ((A * B )/ 2) * C;
+
