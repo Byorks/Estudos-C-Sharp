be8b7f46b9de43cb8794c4cc70cfb9ca27feec37
VANESSA BYORK FERREIRA PINTO <52865485803@info.local>
2024-05-06 16:45:45 -0300

    Added - Ex04Maior

---
+++ Repos/Operadores e Conversões de Dados/Ex04OMaior/Ex04OMaior/Program.cs
@@ -0,0 +1,12 @@
+﻿int A, B, C, MaiorAB, MaiorBC;
+string valores;
+
+valores = Console.ReadLine();
+
+string[] valorel = valores.Split(' ');
+
+A = Convert.ToInt32(valorel[0]);
+
+B
+
+MaiorAB = valorel[0] + valorel[1]; + abs
