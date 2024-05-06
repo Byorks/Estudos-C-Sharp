e23317cf2626e06a3c12e39796d4f144667deb36
VANESSA BYORK FERREIRA PINTO <52865485803@info.local>
2024-05-06 16:17:33 -0300

    Moded - Ex03Area Acomplished

--- Repos/Operadores e Conversões de Dados/Ex03Area/Ex03Area/Program.cs
+++ Repos/Operadores e Conversões de Dados/Ex03Area/Ex03Area/Program.cs
@@ -1,14 +1,17 @@
-﻿double A, B, C;
+﻿using System.Globalization;
+double A, B, C;
 
 double[] calculos= new double[5];
 
+string[] formas = { "Triângulo", "Círculo", "Trapézio", "Quadrado", "Retângulo"}; 
+
 double pi = 3.14159;
 
-A = double.Parse(Console.ReadLine());
+A = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
 
-B = double.Parse(Console.ReadLine());
+B = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
 
-C = double.Parse(Console.ReadLine());
+C = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
 
 //Triangulo base A altura C
 calculos[0] = (A * C) / 2;
@@ -17,5 +20,16 @@ calculos[0] = (A * C) / 2;
 calculos[1] = pi * (C * C);
 
 //area do trapézio com A e B por bases e C por altura
-calculos[2] = ((A * B )/ 2) * C;
+calculos[2] = 0.5 * C * (A + B);
 
+// Area do quadrado com lados B
+calculos[3] = B * B;
+
+// Area do retângulo com lados A e B
+calculos[4] = A * B;
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine(formas[i] +" = "+ calculos[i].ToString("F3",CultureInfo.InvariantCulture));
+
+}
