9b5a89536d5c2d494bad6fc3718ff8e1a4e6d654
Byorks <vanessabyorksp@gmail.com>
2024-05-07 20:38:52 -0300

    Added - Ex05DistanciaEntreDoisPontos Acomplished

---
+++ Repos/Operadores e Conversões de Dados/Ex05DistanciaEntreDoisPontos/Ex05DistanciaEntreDoisPontos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+double x1, y1, x2, y2, distancia;
+
+Console.WriteLine("***** Esse programa calcula a distancia entre dois planos x y ****");
+
+Console.WriteLine("entre P1 x1");
+x1 = Convert.ToDouble(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+Console.WriteLine("entre P1 y1");
+y1 = Convert.ToDouble(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+
+Console.WriteLine("entre P2 x2");
+x2 = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine("entre P2 y2");
+y2 = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+distancia = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+Console.WriteLine("A distancia é: " + distancia.ToString("F4", CultureInfo.InvariantCulture));
