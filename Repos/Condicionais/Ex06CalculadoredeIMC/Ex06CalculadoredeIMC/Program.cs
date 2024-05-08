c7de184e5c64fab5ed00d766683ad616bf6496f2
Byorks <vanessabyorksp@gmail.com>
2024-05-07 21:22:07 -0300

    Added - Ex06CalculadorDeIMC

---
+++ Repos/Condicionais/Ex06CalculadoredeIMC/Ex06CalculadoredeIMC/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+double peso, altura, IMC;
+
+Console.WriteLine("**** Calculdadora de IMC ****");
+
+Console.Write("Nome: ");
+Console.ReadLine();
+
+Console.Write("Peso: ");
+peso = Convert.ToDouble(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+Console.Write("Altura: ");
+altura = Convert.ToDouble(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+IMC = peso / (altura * altura);
+
+IMC = Math.Round(IMC, 2);
+Console.WriteLine("Seu índice de massa corporal é: " + IMC);
