aff551fecac0dcb75fe04638c82865f6aa51ce8a
VANESSA BYORK FERREIRA PINTO <52865485803@info.local>
2024-05-10 16:48:42 -0300

    Added - ExForWhileForEach

---
+++ Repos/Estrutura de Repetição/Ex03ForEach/Ex03ForEach/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] numeros = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+List<int> num = new List<int>();
+
+for (int i = 0; i <= 10; i++)
+{
+    num.Add(i);
+}
+
+
+// Length - método mede o tamanho da lista, então independente de quantos itens tenha ele percorrerá toda.
+// Melhor para procurar algo especifico na lista, em vez de rodar a lista inteira com foreach
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.WriteLine(numeros[i]);
+}
+
+// Roda a lista inteira
+foreach (var n in numeros)
+{
+    Console.WriteLine(n);
+}
---
+++ Repos/Estrutura de Repetição/Ex2Tabuada/Ex2Tabuada/Program.cs
@@ -0,0 +1,10 @@
+﻿int n;
+
+n = int.Parse(Console.ReadLine());
+
+// i conta , entra a comparação, e soma no i, repetindo até a condição não ser atendida
+for (int i = 1; i <= 10; i++)
+{
+    int resultado = i * n;
+    Console.WriteLine(i + " * " + n + " = " + resultado);
+}
