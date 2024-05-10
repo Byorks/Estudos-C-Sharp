0b56f0e59e142046482b1e8ce2039658c9e185a7
Byorks <vanessabyorksp@gmail.com>
2024-05-09 22:43:26 -0300

    Added - Ex09NotasEMoedas

---
+++ Repos/Operadores e Conversões de Dados/Ex09NotasEMoedas/Ex09NotasEMoedas/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+float valor, moedas, nota, sobra;
+int valorInt;
+
+Console.WriteLine("Insira o valor a ser fracionado em cédulas e moedas: ");
+valor = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+nota = 100F;
+sobra = (valor % nota) * 100;
+valor = Math.Round(valor / nota);
+
+Console.WriteLine(valor + " Nota(s) de " + nota);
+
+valor = sobra;
