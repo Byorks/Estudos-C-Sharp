e51af99e7cc9f9b9934583fd92c180612b207c73
Byorks <vanessabyorksp@gmail.com>
2024-05-14 20:32:22 -0300

    Added - Ex101SequenciaDeNumeros

---
+++ Repos/Estrutura de Repetição/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,6 @@
+﻿int num = 3, numB = 0;
+
+while (num !> 0)
+{
+    numB = int.Parse(Console.ReadLine());
+}
---
+++ Repos/Estrutura de Repetição/Ex0101SequenciaDeNumerosESoma/Ex0101SequenciaDeNumerosESoma/Program.cs
@@ -0,0 +1,44 @@
+﻿// Leia um conjunto não determinado de pares
+// de valores M e N (parar quando algum dos
+// valores for menor ou igual a zero). Para cada
+// par lido, mostre a sequência do menor até o
+// maior e a soma dos inteiros consecutivos
+// entre eles (incluindo o N e M).
+
+int numA = 1, numB = 1, soma = 0;
+
+while (numA > 0 && numB > 0) {
+
+    string[] entrada = Console.ReadLine().Split(' ');
+
+    numA = int.Parse(entrada[0]);
+
+    numB = int.Parse(entrada[1]);
+
+    Console.WriteLine($"primeira entrada: {numA}, segunda entrada: {numB}");
+
+    if( numA == 0 || numB == 0)
+    {
+        break;
+    }
+
+    if (numA > numB)
+    {
+        int numC = numB;
+        numB = numA;
+        numA = numC;
+    }
+    
+    for (int cont = numA; cont <= numB; cont++ )
+    {
+        Console.Write($" {cont}");
+        
+        soma += cont;
+        
+    }
+
+    Console.WriteLine($" Soma= {soma}");
+    soma = 0;
+}
+
+Console.WriteLine("Fim do código");
