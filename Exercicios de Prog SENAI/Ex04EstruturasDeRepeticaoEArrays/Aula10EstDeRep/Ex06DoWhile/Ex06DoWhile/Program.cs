abfe266e1af25c0eff1243dbc36ef93aabd3e620
VANESSA BYORK FERREIRA PINTO <52865485803@info.local>
2024-05-15 16:12:59 -0300

    feat: Ex06While

---
+++ Exercicios de Prog SENAI/Ex04EstruturasDeRepeticaoEArrays/Aula10EstDeRep/Ex06DoWhile/Ex06DoWhile/Program.cs
@@ -0,0 +1,22 @@
+﻿//Crie um programa que solicite ao usuário que
+//insira uma senha. O programa deve continuar
+//pedindo a senha até que o usuário insira a senha
+//correta "senha123". Em seguida, exiba uma
+//mensagem de boas-vindas.
+
+bool senhaCorreta = false;
+
+while (!senhaCorreta) {
+    Console.WriteLine("Digite a senha: ");
+    string senha = Console.ReadLine();
+
+    if (senha == "senha123")
+    {
+        Console.WriteLine("Seja bem-vindo(a)");
+        senhaCorreta = true;
+    }
+    else
+    {
+        Console.WriteLine("Senha invalida");
+    }
+}
