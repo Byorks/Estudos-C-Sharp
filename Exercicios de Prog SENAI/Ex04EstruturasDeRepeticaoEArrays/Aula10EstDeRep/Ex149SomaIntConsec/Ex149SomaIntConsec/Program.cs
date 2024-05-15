fa2ced039e9db9a8faddc5b272a77d8fd56f9eb5
VANESSA BYORK FERREIRA PINTO <52865485803@info.local>
2024-05-15 15:18:20 -0300

    feat: Ex149SomaIntConsec Acomplished

---
+++ Exercicios de Prog SENAI/Ex04EstruturasDeRepeticaoEArrays/Aula10EstDeRep/Ex149SomaIntConsec/Ex149SomaIntConsec/Program.cs
@@ -0,0 +1,24 @@
+﻿int A, N, soma = 0;
+
+string[] entrada = Console.ReadLine().Split(' ');
+
+A = int.Parse(entrada[0]);
+
+N = int.Parse(entrada[1]);
+
+soma = A;
+
+while (N <= 0)
+{
+    Console.WriteLine("Digite o valor de N");
+    N = int .Parse(Console.ReadLine());
+
+}
+
+for (int i = 0; i < N; i++)
+{
+    A += i;
+}
+soma += A;
+
+Console.WriteLine("Soma é : " + soma);
