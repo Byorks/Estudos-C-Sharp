caf027df90d9f8a139d7f77e5a9795584bd308cf
Byorks <vanessabyorksp@gmail.com>
2024-05-16 13:56:22 -0300

    feat: exercicio 67

--- SENAI/FundamentosCsharp/Ex04EstruturasDeRepeticaoEArrays/Aula10EstDeRep/Ex67NumerosImpares/Ex67NumerosImpares/Program.cs
+++ SENAI/FundamentosCsharp/Ex04EstruturasDeRepeticaoEArrays/Aula10EstDeRep/Ex67NumerosImpares/Ex67NumerosImpares/Program.cs
@@ -1,3 +1,16 @@
 ﻿//Leia um valor inteiro X (1 <= X <= 1000). Em seguida
 //mostre os ímpares de 1 até X, um valor por linha,
 //inclusive o X, se for o caso.
+
+int X;
+Console.WriteLine("Esse programa mostra os números impares até o valor de X");
+Console.WriteLine("Insira o valor de X, sendo ele maior ou igual a 1 e menor ou igual a 1000");
+X = int .Parse(Console.ReadLine());
+
+for (int i = 1; i < X; i++)
+{
+    if (i % 2 != 0)
+    {
+        Console.WriteLine("valor impar " + i);
+    }
+}
