15f64864e05b28523b3f511307cfd5d7309b92d4
Byorks <vanessabyorksp@gmail.com>
2024-05-16 16:13:05 -0300

    feat: ex72Intervalo2 acomplished

---
+++ SENAI/FundamentosCsharp/Ex04EstruturasDeRepeticaoEArrays/Aula10EstDeRep/Ex72Intervalo2/Program.cs
@@ -0,0 +1,28 @@
+﻿//Leia um valor inteiro N. Este valor será a
+// quantidade de valores inteiros X que serão lidos em
+// seguida.
+// Mostre quantos destes valores X estão dentro do
+// intervalo [10,20] e quantos estão fora do intervalo,
+// mostrando essas informações
+
+int quantRepeticoes, entrada, dentro = 0, fora = 0;
+
+Console.WriteLine("Digite o número de entradas");
+quantRepeticoes = int.Parse(Console.ReadLine());
+
+for (int i = quantRepeticoes; i > 0; i--)
+{
+    entrada = int.Parse(Console.ReadLine());
+
+    if ( entrada >= 10 && entrada <= 20)
+    {
+        dentro++;
+    }
+    else
+    {
+        fora++;
+    }
+}
+
+Console.WriteLine("Dentro - " + dentro);
+Console.WriteLine("Fora - " + fora);
