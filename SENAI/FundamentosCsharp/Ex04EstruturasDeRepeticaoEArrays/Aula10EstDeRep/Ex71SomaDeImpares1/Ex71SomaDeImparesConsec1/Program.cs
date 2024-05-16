6141001c93a787298f626b87775962671bbceb6a
Byorks <vanessabyorksp@gmail.com>
2024-05-16 15:59:12 -0300

    feat: exercicio 71 Soma de Impares - acomplished

---
+++ SENAI/FundamentosCsharp/Ex04EstruturasDeRepeticaoEArrays/Aula10EstDeRep/Ex71SomaDeImpares1/Ex71SomaDeImparesConsec1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Leia 2 valores inteiros X e Y. A seguir, calcule e
+// mostre a somados números impares entre eles
+
+int numA, numB, soma = 0;
+
+Console.WriteLine("Digite o valor de X: ");
+numA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o valor de Y: ");
+numB = int.Parse(Console.ReadLine());
+
+if (numB < numA)
+{
+    int temp = numA;
+    numA = numB;
+    numB = temp;
+}
+
+for (int i = numA + 1; i < numB; i++)
+{
+    if(i % 2 != 0)
+    {   
+        soma += i;
+
+        Console.WriteLine("listando impares: " + i);
+        Console.WriteLine("listando a soma: " + soma);
+
+    }
+}
+
+Console.WriteLine("Valor da soma dos impares = " + soma);
