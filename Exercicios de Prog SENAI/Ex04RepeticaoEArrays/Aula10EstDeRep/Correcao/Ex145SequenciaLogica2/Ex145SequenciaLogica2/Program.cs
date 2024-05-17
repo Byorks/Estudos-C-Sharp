24fd4aca658dbc3ad88f3240bcb9083448bdfad9
VANESSA BYORK FERREIRA PINTO <52865485803@info.local>
2024-05-17 14:41:33 -0300

    feat: exercicio 145 Sequencia Lógica 2

---
+++ Exercicios de Prog SENAI/Ex04RepeticaoEArrays/Aula10EstDeRep/Correcao/Ex145SequenciaLogica2/Ex145SequenciaLogica2/Program.cs
@@ -0,0 +1,19 @@
+﻿string[] valores = Console.ReadLine().Split(' ');
+
+int x = int.Parse(valores[0]);
+
+int y = int.Parse(valores[1]);
+
+for ( int  i = 1; i <= y; i++ )
+{
+    Console.Write(i);
+
+    if ( i % x == 0)
+    {   // pula a linha
+        Console.WriteLine();
+    }
+    else
+    {   // da espaço
+        Console.Write(" ");
+    }
+}
