a246c0c5b24f6c75eccc6105ec71eee8a58b92b8
VANESSA BYORK FERREIRA PINTO <52865485803@info.local>
2024-05-20 15:01:27 -0300

    feat: renomeando pastas

---
+++ SENAI/FundamentosCsharp/Ex04EstruturasDeRepeticaoEArrays/11Array/Exemplos/Ex02Vetores/Ex02Vetores/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+int n = int.Parse(Console.ReadLine());
+double[] vetores = new double[n];
+
+string[] s = Console.ReadLine().Split(' ');
+
+// for para leitura
+for (int i = 0; i < n; i++)
+{   // faz conversão dos valores string para double
+    vetores[i] = double.Parse(s[i], CultureInfo.InvariantCulture);
+}
+
+// for para mostrar o vetor
+for (int i = 0;i < n; i++)
+{
+    Console.WriteLine(vetores[i] + " ");
+    Console.WriteLine();
+}
+
+// for para os calculos
+double soma = 0.0;
+for (int i = 0; i < n; i++)
+{
+    soma += vetores[i];
+}
+
+double media = soma / n;
+
+Console.WriteLine(soma);
+Console.WriteLine(media);
---
+++ SENAI/FundamentosCsharp/Ex04EstruturasDeRepeticaoEArrays/11Array/Exemplos/Vet1/Vet1/Program.cs
@@ -0,0 +1,14 @@
+﻿int n = int.Parse(Console.ReadLine());
+// Coloca o valor de n no valor do tamanho do array
+double[] vetor = new double[n];
+
+// Limita o tamanho do for para o tamanho de n
+for (int i = 0; i < n; i++)
+{
+    vetor[i] = double.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine(vetor[i]);
+}
