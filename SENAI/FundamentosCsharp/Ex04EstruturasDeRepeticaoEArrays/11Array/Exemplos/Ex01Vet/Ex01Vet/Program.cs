30a9e83e101725af560b3b033ddba136d20f5393
VANESSA BYORK FERREIRA PINTO <52865485803@info.local>
2024-05-20 16:49:30 -0300

    feat: exercicio 01 array

---
+++ SENAI/FundamentosCsharp/Ex04EstruturasDeRepeticaoEArrays/11Array/Exemplos/Ex01Vet/Ex01Vet/Program.cs
@@ -0,0 +1,42 @@
+﻿//Faça um programa que leia N números
+//reais e armazene-os em um vetor.
+
+//Em seguida, mostrar na tela o maior
+//número
+//do vetor (supor não haver empates).
+//Mostrar também a posição do maior
+//elemento.dos valores.
+using System.Globalization;
+
+int entradas;
+
+
+Console.WriteLine("Insira o número de entradas");
+entradas = int.Parse(Console.ReadLine());
+
+// criada a lista com o tamanho de entradas
+string[] numS = new string[entradas];
+
+// entradas separadas por espaço
+numS = Console.ReadLine().Split(' ');
+
+double[] numD = new double[entradas];
+
+// Vai fazer a conversão de string para numeros 
+for (int i = 0; i < entradas; i++)
+{
+    numD[i] = double.Parse(numS[i], CultureInfo.InvariantCulture);
+}
+
+double maior = 0, pos = 0;
+for (int i = 0; i < entradas; i++)
+{
+   if (numD[i] > maior )
+    {   
+        maior = numD[i];
+        pos = i;
+    }
+}
+
+Console.WriteLine("O maior da lista é " + maior.ToString("F1") + "\nA sua posição é " + pos);
+
---
+++ SENAI/FundamentosCsharp/Ex04EstruturasDeRepeticaoEArrays/11Array/Exemplos/Ex03/Ex03/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+
+int n = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+string[] nomes = new string[n];
+int[] idades = new int[n];
+double[] alturas = new double[n];
+
+// leitura dos dados
+for (int i = 0; i < n; i++)
+{
+    string[] s = Console.ReadLine().Split('/');
+    nomes[i] = s[0];
+    idades[i] = int.Parse(s[1]);
+    alturas[i] = double.Parse(s[2]);
+}
+
+// calculo da altura media as pessoas
+double soma = 0.0;
+for (int i = 0; i < n; i++)
+{
+    soma += alturas[i];
+}
+Console.WriteLine("Altura média " + soma / n);
+
+// é a mesma coisa que colocar na var e apresentar
+double media = soma / n;
+
+//Porcentagem de pessoas abaixo de 16 anos
+int cont = 0;
+for (int i = 0; i < n; i++)
+{
+    if (idades[i] < 16)
+    {
+        cont++;
+    }
+}
+Console.WriteLine("Pessoas com menos de 16 anos " + cont*100/n + " %");
