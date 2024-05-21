1a86b284405ab5e14220b957697d94c2d016708d
Byorks <vanessabyorksp@gmail.com>
2024-05-21 14:09:32 -0300

    feat: Ex03Vetor

---
+++ SENAI/Fundamentos/Ex04EstDeRepeticaoEArrays/11Array/Ex03Vetor/Program.cs
@@ -0,0 +1,23 @@
+﻿// Faça um programa para ler dois vetores
+// A e B, contendo N elementos cada. Em
+// seguida, gere um terceiro vetor C onde
+// cada elemento de C é a soma dos
+// elementos correspondentes de A e B.
+// Imprima o vetor C gerado.
+
+Console.WriteLine("Insira o número de entradas");
+int entradas = int.Parse(Console.ReadLine());
+
+string[] vetStringA = new string[entradas];
+
+string[] vetStingB = new string[entradas];
+
+int[] vetIntA = new int[entradas];
+
+int[] vetIntB = new int[entradas];
+
+// for para conversão e fazer a soma no mesmo
+for (int i = 0;i < entradas; i++)
+{
+    vetIntA = int.Parse()
+}
