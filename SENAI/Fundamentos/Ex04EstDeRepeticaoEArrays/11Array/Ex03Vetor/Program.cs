196be64881ce7fa7401a34e439e0f13a31a53607
VANESSA BYORK FERREIRA PINTO <52865485803@info.local>
2024-05-22 09:48:10 -0300

    mod: ex03

--- SENAI/Fundamentos/Ex04EstDeRepeticaoEArrays/11Array/Ex03Vetor/Program.cs
+++ SENAI/Fundamentos/Ex04EstDeRepeticaoEArrays/11Array/Ex03Vetor/Program.cs
@@ -6,18 +6,26 @@
 // Imprima o vetor C gerado.
 
 Console.WriteLine("Insira o número de entradas");
+
 int entradas = int.Parse(Console.ReadLine());
 
 string[] vetStringA = new string[entradas];
 
-string[] vetStingB = new string[entradas];
+string[] vetStringB = new string[entradas];
 
 int[] vetIntA = new int[entradas];
 
 int[] vetIntB = new int[entradas];
 
+int [] somaVet = new int [entradas];
+
 // for para conversão e fazer a soma no mesmo
 for (int i = 0;i < entradas; i++)
 {
-    vetIntA = int.Parse()
+    vetIntA[i] = int.Parse(vetStringA[i]);
+    vetIntB[i] = int.Parse(vetStringB[i]);
+
+    somaVet[i] = vetIntA[i] + vetIntB[i];
+
+    Console.Write(somaVet[i] + " ");
 }
