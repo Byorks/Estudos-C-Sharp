671f42337a1bc0e49c6864462576418a9c6203fc
VANESSA BYORK FERREIRA PINTO <52865485803@info.local>
2024-05-22 16:23:22 -0300

    feat: exercicio 07

--- SENAI/Fundamentos/Ex04EstDeRepeticaoEArrays/11Array/Ex07Vet/Ex07Vet/Program.cs
+++ SENAI/Fundamentos/Ex04EstDeRepeticaoEArrays/11Array/Ex07Vet/Ex07Vet/Program.cs
@@ -10,3 +10,38 @@
 // média das notas seja maior ou igual a
 // seis.
 
+using System.Globalization;
+
+int entradas = int.Parse(Console.ReadLine());
+
+string[] entradasNomesNotas = new string[entradas];
+
+string[] listaNomes = new string[entradas];
+
+double[] notasA = new double[entradas];
+
+double[] notasB = new double[entradas];
+
+
+// for para split, divisão
+for (int i = 0; i < entradas; i++)
+{
+    entradasNomesNotas = Console.ReadLine().Split(' ');
+
+    listaNomes[i] = entradasNomesNotas[0];
+    notasA[i] = double.Parse(entradasNomesNotas[1], CultureInfo.InvariantCulture);
+    notasB[i] = double.Parse(entradasNomesNotas[2], CultureInfo.InvariantCulture);
+}
+
+// Aprovados
+double media = 0;
+Console.WriteLine("Lista de aprovados: ");
+for (int i = 0; i < entradas; i++)
+{
+    media = (notasA[i] + notasB[i]) / 2;
+
+    if (media >= 6)
+    {
+        Console.WriteLine(listaNomes[i]);
+    }
+}
