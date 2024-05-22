3c30f3a2ad25ebca83516663fb7bf31da462b52d
VANESSA BYORK FERREIRA PINTO <52865485803@info.local>
2024-05-22 15:03:22 -0300

    feat: exercicio 05

---
+++ SENAI/Fundamentos/Ex04EstDeRepeticaoEArrays/11Array/Ex05Vet/Ex05Vet/Program.cs
@@ -0,0 +1,33 @@
+﻿//Fazer um programa para ler um vetor de
+//N números inteiros. Em seguida, mostrar
+//na tela a média aritmética somente dos
+//números pares lidos.
+
+int entradas = int .Parse(Console.ReadLine());
+
+int[] numLista = new int[entradas];
+
+string[] numListaS = new string[entradas];
+
+numListaS = Console.ReadLine().Split(' ');
+
+
+int soma = 0, cont = 0;
+// for para conversão
+// e média aritmética dos pares
+for (int i = 0; i < entradas; i++)
+{
+    numLista[i] = int.Parse(numListaS[i]);
+
+    if (numLista[i] % 2 == 0 )
+    {   
+        soma += numLista[i];
+        cont++;
+    }
+}
+
+soma /= cont;
+
+Console.WriteLine("Média dos pares = " + soma);
+
+
