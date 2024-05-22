155d5f0e2acf27364cb4eec63d9fd3db5f1ba1ad
VANESSA BYORK FERREIRA PINTO <52865485803@info.local>
2024-05-22 16:47:19 -0300

    feat: exercicio 08

---
+++ SENAI/Fundamentos/Ex04EstDeRepeticaoEArrays/11Array/Ex08Vet/Ex08Vet/Program.cs
@@ -0,0 +1,45 @@
+﻿// Tem - se um conjunto de dados contendo
+// a altura e o sexo (M, F) de N pessoas.
+// Fazer um programa que calcule e
+// escreva:
+// ✓a maior e a menor altura do grupo
+// ✓a média de altura das mulheres
+// ✓o número de homens
+
+int entradas = int.Parse(Console.ReadLine());
+
+string[] entradaAltSex = new string[entradas];
+
+string[] sexo = new string[entradas];
+
+double[] altura = new double[entradas];
+
+// dividir
+for (int i = 0; i < entradas; i++)
+{
+    entradaAltSex = Console.ReadLine().Split(' ');
+
+    altura[i] = double.Parse(entradaAltSex[0]);
+
+    sexo[i] = entradaAltSex[1];
+}
+
+// maior e menor
+double maior = 0, menor = altura[0], posMa = 0, posMe = 0;
+for (int i = 0; i < entradas; i++)
+{
+    if (altura[i] > maior)
+    {
+        maior = altura[i];
+        posMa = i;
+    }
+    if (altura[i] < menor)
+    {
+        menor = altura[i];
+
+        posMe = i;
+    }
+
+}
+
+Console.WriteLine("Menor altura: " +)
