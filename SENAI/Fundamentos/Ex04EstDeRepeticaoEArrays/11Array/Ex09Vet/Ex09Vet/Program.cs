f40b5b5125c7a9dae8274d88f1e778692917ffc6
VANESSA BYORK FERREIRA PINTO <52865485803@info.local>
2024-05-23 14:04:43 -0300

    feat: exercicio 09 vet

---
+++ SENAI/Fundamentos/Ex04EstDeRepeticaoEArrays/11Array/Ex09Vet/Ex09Vet/Program.cs
@@ -0,0 +1,13 @@
+﻿
+int entradas = int.Parse(Console.ReadLine());
+
+string[] itemCompraVenda = new string[entradas];
+
+string[] item = new string[entradas];
+
+double[] compra = new double[entradas];
+
+double[] vendas = new double[entradas];
+
+// entradas
+itemCompraVenda = Console.ReadLine().Split(' ');
