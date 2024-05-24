e252e8933d16cb45ad82e24db9246e7ee81de870
VANESSA BYORK FERREIRA PINTO <52865485803@info.local>
2024-05-24 16:34:38 -0300

    feat: exercicio 09 correcao

---
+++ SENAI/Fundamentos/Ex04EstDeRepeticaoEArrays/11Array/Correcao/Ex08Vet/Ex08Vet/Program.cs
@@ -0,0 +1,65 @@
+﻿// Tem - se um conjunto de dados contendo
+// a altura e o sexo (M, F) de N pessoas.
+// Fazer um programa que calcule e
+// escreva:
+// ✓a maior e a menor altura do grupo
+// ✓a média de altura das mulheres
+// ✓o número de homens
+
+using System.Globalization;
+
+int entradas = int.Parse(Console.ReadLine());
+
+string[] entradaAltSex = new string[entradas];
+
+string[] sexo = new string[entradas];
+
+double[] altura = new double[entradas];
+
+// dividir
+for (int i = 0; i < entradas; i++)
+{
+    entradaAltSex = Console.ReadLine().Split(' ');
+
+    altura[i] = double.Parse(entradaAltSex[0], CultureInfo.InvariantCulture);
+
+    sexo[i] = entradaAltSex[1];
+}
+
+// maior e menor
+double maior = 0, menor = altura[0], posMa = 0, posMe = 0;
+for (int i = 0; i < entradas; i++)
+{
+    if (altura[i] > maior)
+    {
+        maior = altura[i];
+        posMa = i;
+    }
+    if (altura[i] < menor)
+    {
+        menor = altura[i];
+
+        posMe = i;
+    }
+
+}
+
+Console.WriteLine("Menor altura: " + menor.ToString(CultureInfo.InvariantCulture));
+
+Console.WriteLine("Maior altura: " + maior.ToString(CultureInfo.InvariantCulture));
+
+// media
+double mediaF = 0, numF = 0;
+for(int i = 0; i < entradas; i++){
+    if (sexo[i].ToLower() == "f") {
+        mediaF += altura[i];
+        numF++;
+    }
+}
+mediaF /= numF;
+
+Console.WriteLine("Media das alturas das mulheres : " + mediaF.ToString("F2", CultureInfo.InvariantCulture));
+
+// Numero de homens 
+
+Console.WriteLine($"Número de homens : {sexo.Length - numF}");
---
+++ SENAI/Fundamentos/Ex04EstDeRepeticaoEArrays/11Array/Correcao/Ex09Vetor/Program.cs
@@ -0,0 +1,81 @@
+﻿// Um comerciante deseja fazer o levantamento
+// do lucro das mercadorias que ele
+// comercializa. Para isto, mandou digitar
+// um conjunto de N mercadorias, cada uma
+// contendo nome, preço de compra e preço de
+// venda das mesmas.
+// Fazer um programa que leia tais dados e
+// determine e escreva quantas mercadorias
+// proporcionaram:
+// ✓ lucro < 10 %
+// ✓ 10 % ≤ lucro ≤ 20%
+// ✓ lucro> 20%
+// Determine e escreva também o valor total de
+// compra e de venda de todas as mercadorias,
+// assim como o lucro total.
+
+using System.Globalization;
+
+int n = int.Parse(Console.ReadLine());
+
+string[] item = new string[n];
+double[] compra = new double[n];
+double[] vendas = new double[n];
+
+
+
+
+// Distribuir as listas
+for (int i = 0; i < n; i++)
+{
+    string[] valores = Console.ReadLine().Split(' ');
+
+    item[i] = valores[0];
+    compra[i] = double.Parse(valores[1], CultureInfo.InvariantCulture);
+    vendas[i] = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+}
+
+int contAbaixoDe10 = 0;
+int contEntre10e20 = 0;
+int contAcimaDe20 = 0;
+
+for (int i = 0; i < n; i++)
+{
+    double lucro = vendas[i] - compra[i];
+    double porcentagemDeLucro = lucro / compra[i] * 100.0;
+
+    if ( porcentagemDeLucro < 10.0 )
+    {
+        contAbaixoDe10++;
+
+    }else if ( porcentagemDeLucro <= 20.0)
+    {
+        contEntre10e20++;
+    }else
+    {
+        contAcimaDe20++;
+    }
+}
+
+Console.WriteLine("Lucro abaixo de 10%: " + contAbaixoDe10);
+Console.WriteLine("Lucro entre 10% e 20%: " + contEntre10e20);
+Console.WriteLine("Lucro acima de 20%: " + contAcimaDe20);
+
+
+double totalCompra = 0.0;
+double totalVenda = 0.0;
+
+for (int i = 0; i < n;i++)
+{
+    totalCompra += compra[i];
+    totalVenda += vendas[i];
+}
+
+double totalLucro = totalVenda - totalCompra;
+
+Console.WriteLine("Valor total de compra: " + totalCompra.ToString("F2", CultureInfo.InvariantCulture));
+Console.WriteLine("Valor total de venda: " + totalVenda.ToString("F2", CultureInfo.InvariantCulture));
+Console.WriteLine("Lucro total: " + totalLucro.ToString("F2", CultureInfo.InvariantCulture));
+
+
