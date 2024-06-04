42c594cf0a723980da445c8c8548c3830e2b767f
Byorks <vanessabyorksp@gmail.com>
2024-06-03 21:24:42 -0300

    feat: metodos

---
+++ Cursos/Udemy/GAP/POO/POO/MetodoDasClasses/Metodos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodoDasClasses
+{
+    internal class Metodos
+    {
+        // Método Simples 
+        public void Cumprimentar()
+        {
+            Console.WriteLine("Olá, seja bem vindo(a).");
+        }
+
+        // Métodos com Parâmetros
+        // Pode ter retorno
+        // Dentro dos () entrará os parametros, ficarão visiveis apenas dentro do escopo
+        public void Somar(int num1, int num2)
+        {
+            int resultado = num1 + num2;
+            Console.WriteLine("A soma é: " + resultado);
+        }
+
+        public void Apresentar (string nome, int idade)
+        {
+            Console.WriteLine("Meu nome é " + nome + " e tenho " + idade + " anos");
+        }
+    }
+}
---
+++ Cursos/Udemy/GAP/POO/POO/MetodoDasClasses/Program.cs
@@ -0,0 +1,11 @@
+﻿using MetodoDasClasses;
+
+Metodos m = new Metodos();
+m.Cumprimentar();
+
+// Os valores devem ser do tipo definido na criação do método, e também é possível colocar varíaveis do mesmo tipo
+m.Somar(10, 5);
+
+m.Apresentar("Dioguinho", 23);
+
+Console.ReadKey();
