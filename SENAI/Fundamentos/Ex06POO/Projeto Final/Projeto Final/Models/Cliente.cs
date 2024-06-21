e7f8ec25ff1e5a9f70ebe9076d34bfefd812e8ac
VANESSA BYORK FERREIRA PINTO <52865485803@info.local>
2024-06-21 16:45:48 -0300

    Feat Client Metodo CriarCliente

--- SENAI/Fundamentos/Ex06POO/Projeto Final/Projeto Final/Models/Cliente.cs
+++ SENAI/Fundamentos/Ex06POO/Projeto Final/Projeto Final/Models/Cliente.cs
@@ -11,6 +11,38 @@ namespace Projeto_Final.Models
         public string Endereco { get; set; }
         public int Telefone { get; set; }
         // Vou pegar esse valor separadamente e depois atribuir aqui!
-        public int DataNascimento { get; set; }
+        public int DiaNascimento { get; set; }
+        public int MesNascimento { get; set; }
+        public int AnoNascimento { get; set; }
+
+        public override Cliente AdicionarPessoa()
+        {
+            Cliente cliente = new Cliente();
+
+            Console.WriteLine(" Criação de Cliente");
+
+            Console.WriteLine("Digite o nome: ");
+            cliente.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o CPF: ");
+            cliente.Id = int.Parse(Console.ReadLine());
+
+            string[] dataNascimento = new string[3];
+            Console.WriteLine("Digite sua data de nascimento separado por / :");
+            dataNascimento = Console.ReadLine().Split('/');
+            cliente.DiaNascimento = int.Parse(dataNascimento[0]);
+            cliente.MesNascimento = int.Parse(dataNascimento[1]);
+            cliente.AnoNascimento = int.Parse(dataNascimento[2]);
+
+            Console.WriteLine("Informe o endereço: ");
+            cliente.Endereco = Console.ReadLine();
+
+            Console.WriteLine("Informe um número de telefone: ");
+            cliente.Telefone = int.Parse(Console.ReadLine());
+
+            return cliente;
+        }
+
+
     }
 }
---
+++ SENAI/Fundamentos/Ex06POO/Projeto Final/Projeto Final/Services/ClienteService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Final.Services
+{
+    using Models;
+    public class ClienteService
+    {
+        // Criando lista do Tipo Clientes com nome _clientes
+        List<Cliente> _clientes = new List<Cliente>();
+
+        // Criando Cliente
+        public Cliente Cliente();
+
+        // Atualizando Cliente
+
+        // Deletando CLiente
+
+        // Listando Clientes
+            
+        
+
+    }
+}
