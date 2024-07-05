f85d56b676fb8f79941f404068ff0521f6ddc3ab
Byorks <vanessabyorksp@gmail.com>
2024-07-05 14:30:29 -0300

    :sparkles: feat: Estudando Objetos e Método Construtor

---
+++ Cursos/Curso em Video/POO/Aula 03/Aula 04/Caneta.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_04
+{
+    public class Caneta
+    {
+        public string Modelo { get; set; }
+        private float Ponta { get; set; }
+        private bool Tampada { get; set; }
+        private string Cor {  get; set; }
+
+        // Método Construtor - Assim que eu criar o objeto, esses comandos serão automáticamente executados
+        public Caneta()
+        {
+            Cor = "Azul";
+            Tampar();
+        }
+
+        public void GetModelo() 
+        {
+            Console.WriteLine(Modelo); 
+        }
+
+        public void SetModelo(string m)
+        {
+            Modelo = m;
+        }
+
+        public void GetPonta()
+        {
+            Console.WriteLine(Ponta);
+        }
+
+        public void SetPonta(float p) 
+        {
+            Ponta = p;
+        }
+
+        public void Tampar()
+        {
+            Console.WriteLine($"Caneta está tampada");
+            Tampada = true;
+        }
+        public static void MostarPropriedades(Caneta caneta)
+        {
+            Type type = caneta.GetType();
+            PropertyInfo[] propInfo = type.GetProperties();
+            foreach (PropertyInfo prop in propInfo)
+            {
+                Console.WriteLine($"{prop.Name}: {prop.GetValue(caneta)} "); // Com o modelo privado, não consigo printa-lo
+            }
+
+        }
+
+        public static void MostrarDetalhes(Caneta caneta) 
+        {
+            foreach (var prop in caneta)
+            {
+
+            }
+        }
+    }
+}
---
+++ Cursos/Curso em Video/POO/Aula 03/Aula 04/Program.cs
@@ -0,0 +1,18 @@
+﻿using Aula_04;
+
+Caneta c1 = new();
+
+//c1.SetModelo("BIC");
+c1.Modelo = "NIC";
+//c1.Ponta = 0.6f; Não é possível por conta da proteção
+c1.SetPonta(0.5f);
+
+Caneta.MostarPropriedades(c1);
+
+Caneta.MostrarDetalhes(c1);
+
+Console.WriteLine();
+
+Caneta c2 = new();
+
+Caneta.MostrarDetalhes(c2);
